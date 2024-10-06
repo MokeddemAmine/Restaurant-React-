f957173c6915b6ba14c3ff306fb6a02ed96c382c
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-10-06 17:54:45 +0200

    design why choose us section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Banner from './components/Banner/Banner';
+import ChooseUs from './components/ChooseUs/ChooseUs';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Banner/>
+      <ChooseUs/>
     </div>
   );
 }
---
+++ src/components/ChooseUs/ChooseUs.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import {FiCoffee } from 'react-icons/fi'
+import { GiKnifeFork } from "react-icons/gi";
+import { MdRestaurantMenu } from "react-icons/md";
+import { FaUtensils } from "react-icons/fa";
+
+
+export default function ChooseUs() {
+  return (
+    <div className='py-5' style={{backgroundColor:'rgb(249 250 251)'}}>
+      <div className="container py-5">
+        <h2 className="text-center text-dark text-capitalize fs-2">why choose us</h2>
+        <div className="row my-5">
+            <div className="col-md-6 col-xl-3">
+                <div className='d-flex flex-column text-center justify-content-center align-items-center'>
+                    <p className='text-gray fw-semibold'>Enjoy a variety of freshly brewed coffee and refreshing beverages.</p>
+                    <p className='rotate-90 text-primary fw-bold fs-3'>....</p>
+                    <FiCoffee className='text-primary fs-1'/>
+                </div>
+            </div>
+            <div className="col-md-6 col-xl-3">
+                <div className='d-flex flex-column-reverse text-center justify-content-center align-items-center'>
+                    <p className='text-gray fw-semibold'>Indulge in a delightful dining experience with carefully crafted dishes.</p>
+                    <p className='rotate-90 text-secondary fw-bold fs-3'>....</p>
+                    <GiKnifeFork className='text-secondary fs-1'/>
+                </div>
+            </div>
+            <div className="col-md-6 col-xl-3">
+                <div className='d-flex flex-column text-center justify-content-center align-items-center'>
+                    <p className='text-gray fw-semibold'>Explore our diverse menu, offering a selection of appetizers, mains, and desserts.</p>
+                    <p className='rotate-90 text-primary fw-bold fs-3'>....</p>
+                    <MdRestaurantMenu className='text-primary fs-1'/>
+                </div>
+            </div>
+            <div className="col-md-6 col-xl-3">
+                <div className='d-flex flex-column-reverse text-center justify-content-center align-items-center'>
+                    <p className='text-gray fw-semibold'>Order your favorite meals with ease and enjoy fast, convenient service.</p>
+                    <p className='rotate-90 text-secondary fw-bold fs-3'>....</p>
+                    <FaUtensils className='text-secondary fs-1'/>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
